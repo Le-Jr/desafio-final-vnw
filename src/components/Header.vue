8fbaba30ea96a4452679199f3f2e760b8dc265c5
LE-Jr <nascimentoleandrojr@gmail.com>
2024-12-11 19:44:33 -0300

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<script setup>
+import logo from "../assets/logo.png";
+</script>
+
+<template>
+  <header>
+    <img :src="logo" alt="Logo do site" />
+    <nav>
+      <ul>
+        <li>Início</li>
+        <li>Artigos</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  width: 100%;
+  /* border: red solid 1px; */
+  height: 5rem;
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  font-size: 1rem;
+  padding: 0 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  img {
+    /* width: 60px; */
+    height: 30px;
+  }
+  nav {
+    width: 200px;
+    /* border: solid blue 5px; */
+  }
+  ul {
+    /* border: red solid 1px; */
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    gap: 1em;
+    list-style: none;
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,100 @@
+<script setup>
+import banner from "../assets/banner.png";
+import card01 from "../assets/card01.png";
+</script>
+
+<template>
+  <main>
+    <section class="banner">
+      <img :src="banner" alt="" />
+      <h1>PROGRAMAÇÃO</h1>
+    </section>
+    <h3>Artigos Recomendados</h3>
+    <section class="artigos">
+      <div class="card">
+        <img :src="card01" alt="" />
+        <h3>O que é linguagem de programação? Conheça as principais</h3>
+        <p>
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de...
+        </p>
+      </div>
+      <div class="card">
+        <img :src="card01" alt="" />
+        <h3>O que é linguagem de programação? Conheça as principais</h3>
+        <p>
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de...
+        </p>
+      </div>
+      <div class="card">
+        <img :src="card01" alt="" />
+        <h3>O que é linguagem de programação? Conheça as principais</h3>
+        <p>
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de...
+        </p>
+      </div>
+      <div class="card">
+        <img :src="card01" alt="" />
+        <h3>O que é linguagem de programação? Conheça as principais</h3>
+        <p>
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de...
+        </p>
+      </div>
+      <div class="card">
+        <img :src="card01" alt="" />
+        <h3>O que é linguagem de programação? Conheça as principais</h3>
+        <p>
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de...
+        </p>
+      </div>
+      <div class="card">
+        <img :src="card01" alt="" />
+        <h3>O que é linguagem de programação? Conheça as principais</h3>
+        <p>
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de...
+        </p>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.banner {
+  position: relative;
+  h1 {
+    color: white;
+    font-weight: 700;
+    position: absolute;
+    top: 150px;
+    left: 600px;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
+  width: 100%;
+}
+
+.artigos {
+  margin-top: 3em;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  gap: 2em;
+
+  .card {
+    border: solid 1px black;
+    img {
+      width: 100%;
+    }
+    width: 30%;
+    height: 50%;
+  }
+}
+</style>
